aac0f4c9028725166e5728f139ba71ea5fd45404
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2025-01-09 15:45:13 +0100

    Fix sitemap naming error

--- app/server-sitemap-index.xml/route.ts
+++ app/server-sitemap-index.xml/route.ts
@@ -1,6 +1,6 @@
 import { getServerSideSitemapIndex } from "next-sitemap";
 
-export async function GET(request: Request) {
+export default async function GET(request: Request) {
   return getServerSideSitemapIndex([
     `${process.env.SITE_URL}/server-sitemap.xml`,
   ]);
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://mysticfilms.vercel.app",
   generateRobotsTxt: true,
-  exclude: ["/blog/*"],
+  exclude: ["/server-sitemap-index.xml"],
   robotsTxtOptions: {
     additionalSitemaps: [`${process.env.SITE_URL}/server-sitemap-index.xml`],
   },
